98ef3de969a25359e3c58743d9adaeb424030d9e
nordicebear <nordicebeargames@gmail.com>
2019-04-03 16:09:04 +0300

    Number Guesser v0.1(creating algoritm)

---
+++ Number Guesser/Scripts/Game.gd
@@ -0,0 +1,30 @@
+extends Node
+
+# 0 ~ 1000
+var guess
+var min_guessed = 0
+var max_guessed = 1000
+
+func _ready():
+	guess = (min_guessed + max_guessed) / 2
+	print("-------------------------")
+	print("-------------------------")
+	print("-------------------------")
+	print("Hello from Number Guesser!")
+	print("I'll guess any number that you think between 0 and 1000")
+	print("Is" +" "+ str(guess) +" "+ "your number ?")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("up"): # works if just pressed "up". (not every frame)
+		_try_guess("up")
+	elif Input.is_action_just_pressed("down"):
+		_try_guess("down")
+
+#type should be "up" = greater or "down" = lesser
+func _try_guess(type):
+	if type == "up":
+		min_guessed = guess
+	else:
+		max_guessed = guess
+	guess = (min_guessed + max_guessed) / 2
+	print("Is" +" "+ str(guess) +" "+ "your number ?")
