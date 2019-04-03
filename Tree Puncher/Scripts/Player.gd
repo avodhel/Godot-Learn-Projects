15d40e521d1e06ab6795452ab7325b0baea5055e
nordicebear <nordicebeargames@gmail.com>
2019-04-03 23:52:14 +0300

    Tree Puncher v0.5(moving player side by side)

--- Tree Puncher/Scripts/Player.gd
+++ Tree Puncher/Scripts/Player.gd
@@ -2,10 +2,22 @@ extends Node2D
 
 onready var animation = $Animation
 
+var center
+var offset
+
 func _ready():
-	pass
+	center = get_viewport_rect().size.x / 2 #center of the screen
+	offset = abs(position.x - center)
 	
 func _input(event):
 	#if mouse clicked or screen touched
 	if (event is InputEventMouseButton or event is InputEventScreenTouch) and event.is_pressed():
+		
+		if event.position.x < center: #if player is on the left side of the screen
+			position.x = center - offset
+			scale.x = -abs(scale.x)
+		else:
+			position.x = center + offset
+			scale.x = abs(scale.x)
+			
 		animation.play("punch")
