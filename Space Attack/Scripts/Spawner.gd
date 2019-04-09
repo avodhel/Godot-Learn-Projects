7b6fa3e2af99bfee4c238bf7a95928692133254f
nordicebear <nordicebeargames@gmail.com>
2019-04-09 16:09:58 +0300

    Space Attack v0.6(spawn enemies)

---
+++ Space Attack/Scripts/Spawner.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export (PackedScene) var formation
+
+var center 
+var offset = 100
+
+func _ready():
+	center = get_viewport().get_visible_rect().size.x / 2
+	_spawn_formation()
+
+func _process(delta):
+	pass
+	
+func _spawn_formation():
+	randomize() # more random
+	var position_x = rand_range(center - offset, center + offset)
+	var position_y = -150
+	var new_formation = formation.instance()
+	new_formation.position = Vector2(position_x, position_y)
+	add_child(new_formation)
