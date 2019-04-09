35e6be87ac16b93e265a636fcf7ad187d030915c
nordicebear <nordicebeargames@gmail.com>
2019-04-09 18:00:51 +0300

    Space Attack v0.8(explode our ship and restart game when this happens)

--- Space Attack/Scripts/EnemyProjectile.gd
+++ Space Attack/Scripts/EnemyProjectile.gd
@@ -4,13 +4,14 @@ export var speed = 450
 export var damage = 10
 
 func _ready():
-	connect("area_entered", self, "_on_area_entered")
+	connect("body_entered", self, "_on_body_entered")
 
 func _process(delta):
 	position.y += speed * delta
 	if position.y >= 2000:
 		queue_free()
 	
-func _on_area_entered(area):
-	if area.name == "Player":
-		area.add_damage(damage)
+func _on_body_entered(body):
+	if body.name == "Player":
+		body.add_damage(damage)
+		queue_free()
--- Space Attack/Scripts/Player.gd
+++ Space Attack/Scripts/Player.gd
@@ -1,14 +1,17 @@
 extends KinematicBody2D
 
 export var speed = 500
+export var health = 100
 export (PackedScene) var projectile
 
 onready var sprite = $Sprite
 onready var shoot_timer = $ShootTimer
+onready var restart_timer = $RestartTimer
 
 var screensize
 var half_sprite_size
 var can_shoot = true
+var dead = false
 
 func _ready():
 	screensize = get_viewport_rect().size
@@ -31,3 +34,17 @@ func _process(delta):
 
 func _on_ShootTimer_timeout():
 	can_shoot = true
+	
+func add_damage(damage):
+	if dead:
+		return
+	health -= damage
+	if health <= 0:
+		dead = true
+		health = 0
+		restart_timer.start()
+		set_process(false)
+		sprite.queue_free()
+
+func _on_RestartTimer_timeout():
+	get_tree().reload_current_scene()
