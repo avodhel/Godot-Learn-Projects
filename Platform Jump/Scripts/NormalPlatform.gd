beaf3217a900b56477b24435cd86a15e235bae4e
nordicebear <nordicebeargames@gmail.com>
2019-04-10 13:25:46 +0300

    Platform Jump v0.3(creating the platforms)

---
+++ Platform Jump/Scripts/NormalPlatform.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready():
+	connect("body_entered", self, "_on_body_entered") 
+	
+func _process(delta):
+	pass
+	
+func _on_body_entered(body):
+	if body.name == "Player":
+		if body.position.y < position.y:
+			body.jump()
