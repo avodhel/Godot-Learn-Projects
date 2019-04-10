8e2c57035fe4d3511323d554c287c4e38f41281a
nordicebear <nordicebeargames@gmail.com>
2019-04-10 16:21:01 +0300

    Platform Jump v0.7(spawning platforms)

---
+++ Platform Jump/Scripts/Game.gd
@@ -0,0 +1,51 @@
+extends Node
+
+export (Array, PackedScene) var platforms
+
+const MIN_INTERVAL = 100 #min interval between two platforms
+const MAX_INTERVAL = 250 #max interval between two platforms
+const INITIAL_PLATFORMS_COUNT = 40 #max platform number on the screen
+
+var current_min_interval
+var current_max_interval
+var last_spawn_height
+var screen_size
+
+func _ready():
+	last_spawn_height = get_viewport().get_visible_rect().size.y
+	screen_size = get_viewport().get_visible_rect().size.x
+	current_min_interval = MIN_INTERVAL
+	current_max_interval = MAX_INTERVAL
+	_spawn_first_platforms()
+
+func _process(delta):
+	pass
+	
+func _spawn_first_platforms():
+	for counter in range(INITIAL_PLATFORMS_COUNT):
+		_spawn_platform()
+
+func _spawn_platform():
+	var index
+	var new_platform
+	
+	index = rand_range(0, platforms.size())
+	new_platform = platforms[index].instance()
+	add_child(new_platform)
+	var spawn_x = rand_range(0+ new_platform.sprite_half_width, screen_size - new_platform.sprite_half_width)
+	var spawn_position = Vector2(spawn_x, last_spawn_height)
+	new_platform.position = spawn_position
+	last_spawn_height -= rand_range(current_min_interval, current_max_interval)
+	current_min_interval += 5
+	current_max_interval += 7.5
+	current_min_interval = clamp(current_min_interval, MIN_INTERVAL, MAX_INTERVAL / 0.75)
+	current_max_interval = clamp(current_max_interval, MIN_INTERVAL, MAX_INTERVAL)
+	
+	
+	
+	
+	
+	
+	
+	
+	
