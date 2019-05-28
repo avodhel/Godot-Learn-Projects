b119545e3a1aba818eca3694c6674c8a429b93f5
nordicebear <nordicebeargames@gmail.com>
2019-05-28 16:37:46 +0300

    Demo Project v0.10

--- Demo Project/demo/MyDemoObject.gd
+++ Demo Project/demo/MyDemoObject.gd
@@ -6,6 +6,10 @@ onready var demo1_scene = "res://demo/demo.tscn"
 
 var velocity = Vector2()
 var speed = 100
+var moves = {'ui_up' : Vector2(0, -1),
+             'ui_down': Vector2(0, 1),
+             'ui_left': Vector2(-1, 0),
+             'ui_right': Vector2(1, 0)}
 
 func _ready():
 	_chose_object()
@@ -20,15 +24,11 @@ func _chose_object():
 
 func _control(delta):
 	velocity = Vector2()
-	if Input.is_action_pressed("ui_up"):
-		velocity.y  -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y  += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x  -= 1
-	if Input.is_action_pressed("ui_right"):
-		velocity.x  += 1
 	
+	for dir in moves.keys():
+		if Input.is_action_pressed(dir):
+			velocity = moves[dir]
+
 	velocity  = velocity.normalized()  * speed * delta
 	self.position += velocity
 
